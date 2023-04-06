1fbf99d35c0776b0bcd25ee1c4ddb232942e308a
Reza Qavi <75136522+RezaQavi-Git@users.noreply.github.com>
2023-04-07 02:10:31 +0330

    Update install_requirnment.sh

--- install_requirnment.sh
+++ install_requirnment.sh
@@ -4,6 +4,7 @@ echo "INSTALLING DOCKER"
 sudo apt-get -y update 
 apt-get install -y ca-certificates curl gnupg lsb-release 
 mkdir -p /etc/apt/keyrings
+rm -f /etc/apt/keyrings/docker.gpg
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update -y
