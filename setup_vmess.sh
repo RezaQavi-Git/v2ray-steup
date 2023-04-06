0f1186f386384852d365baa77c1a5d6518817d9f
ReZa QaVi <reza.qavi@tapsi.cab>
2023-04-06 22:10:59 +0330

    Fix bugs

--- setup_vmess.sh
+++ setup_vmess.sh
@@ -11,13 +11,13 @@ apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo docker run hello-world
 
 echo "INSTALLING NECESSARY APT"
-apt install -y jq nano uuidgen
+apt install -y jq nano uuid-runtime
 
 echo "GENERATE RANDOM UUID"
 uuid=$(uuidgen)
 
 echo "GENERATE VMESS CONFIG FILE"
-jq --arg uuid "$uuid" '.inbounds[0].settings.clients[0].id = $uuid' /configs/vmess.json > /root/vmess_config.json
+jq --arg uuid "$uuid" '.inbounds[0].settings.clients[0].id = $uuid' ./configs/vmess.json > /root/vmess_config.json
 
 echo "DOCKER RUN COMMAND"
 docker run -d --restart always --log-driver=none --network host -e V2RAY_VMESS_AEAD_FORCED=false -v /root/vmess_config.json:/etc/v2ray/config.json:ro v2fly/v2fly-core run -config /etc/v2ray/config.json
