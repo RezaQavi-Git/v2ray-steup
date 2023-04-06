ccd580d7e210334af98c3973f7730fe794654062
Reza Qavi <75136522+RezaQavi-Git@users.noreply.github.com>
2023-04-07 02:14:25 +0330

    Update install_requirnment.sh

--- install_requirnment.sh
+++ install_requirnment.sh
@@ -9,7 +9,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update -y
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo docker run hello-world
+docker run hello-world
 
 echo "INSTALLING NECESSARY APT"
 apt install -y jq nano uuid-runtime
