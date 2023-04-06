76930205bdd29996b59a209cf76faec7c8111d3d
ReZa QaVi <reza.qavi@tapsi.cab>
2023-04-07 00:32:54 +0330

    Fix path

--- setup_vmess.sh
+++ setup_vmess.sh
@@ -4,7 +4,7 @@ echo "GENERATE RANDOM UUID"
 uuid=$(uuidgen)
 
 echo "GENERATE VMESS CONFIG FILE"
-jq --arg uuid "$uuid" --arg port "$1" '.inbounds[0].settings.clients[0].id = $uuid | .inbounds[0].port = $port'  ./config.json > ./"vmess_$1_config".json
+jq --arg uuid "$uuid" --arg port "$1" '.inbounds[0].settings.clients[0].id = $uuid | .inbounds[0].port = $port'  ./configs/vmess.json > /root/"vmess_$1_config".json
 
 echo "DOCKER RUN COMMAND"
 docker run -d --restart always --log-driver=none --network host -e V2RAY_VMESS_AEAD_FORCED=false -v /root/"vmess_$1_config".json:/etc/v2ray/config.json:ro v2fly/v2fly-core run -config /etc/v2ray/config.json
