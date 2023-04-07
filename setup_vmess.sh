87770ccea97bf8b53560a81c7d0a35fba5a2f6d8
ReZa QaVi <reza.qavi@tapsi.cab>
2023-04-07 14:23:40 +0330

    Fix and add readme

--- setup_vmess.sh
+++ setup_vmess.sh
@@ -9,7 +9,7 @@ jq --arg uuid "$uuid" --arg port "$1" '.inbounds[0].settings.clients[0].id = $uu
 echo "DOCKER RUN COMMAND"
 docker run -d --restart always --log-driver=none --network host -e V2RAY_VMESS_AEAD_FORCED=false -v /root/"vmess_$1_config".json:/etc/v2ray/config.json:ro v2fly/v2fly-core run -config /etc/v2ray/config.json
 
-echo "DETAIL YOU WILL NEED"
+echo "DEFAULT YOU WILL NEED"
 echo address = {YOUR SERVER IP ADDRESS}
 echo port = $1
 echo uuid = $uuid
