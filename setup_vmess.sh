503dbcab4c103614781ee467e6d7767aa56ffc88
Reza Qavi <75136522+RezaQavi-Git@users.noreply.github.com>
2023-04-08 18:28:11 +0330

    Update setup_vmess.sh

--- setup_vmess.sh
+++ setup_vmess.sh
@@ -5,13 +5,13 @@ uuid=$(uuidgen)
 
 echo "GENERATE VMESS CONFIG FILE"
 mkdir -p /root/configs
-jq --arg uuid "$uuid" --arg port "$1" '.inbounds[0].settings.clients[0].id = $uuid | .inbounds[0].port = $port'  ./configs/vmess.json > /root/configs/"vmess_$1_config".json
+jq --arg uuid "$uuid" --arg port "$1" --arg path "$2" '.inbounds[0].settings.clients[0].id = $uuid | .inbounds[0].port = $port | .inbounds[0].streamSettings.wsSettings.path = "/"+$path '  ./configs/vmess.json > /root/configs/"vmess_$1_$2_config".json
 
 echo "DOCKER RUN COMMAND"
-docker run -d --restart always --log-driver=none --network host -e V2RAY_VMESS_AEAD_FORCED=false -v /root/configs/"vmess_$1_config".json:/etc/v2ray/config.json:ro v2fly/v2fly-core run -config /etc/v2ray/config.json
+docker run -d --restart always --log-driver=none --network host -e V2RAY_VMESS_AEAD_FORCED=false -v /root/configs/"vmess_$1_$2_config".json:/etc/v2ray/config.json:ro v2fly/v2fly-core run -config /etc/v2ray/config.json
 
 echo "DEFAULT YOU WILL NEED"
 echo address = {YOUR SERVER IP ADDRESS}
 echo port = $1
+echo path = $2
 echo uuid = $uuid
-echo path = '/cookie'
